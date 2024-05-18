923a7c063e9efedc415c734b64d6b18b4c2ae7e6
toaderhackeru romania <mateitoader80@gmail.com>
2024-05-18 10:39:07 +0000

    added cmakefile and restrucured the code

---
+++ CMakeFiles/neural-network.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neural-network.dir/src/NeuralNetwork.cpp.o"
+  "CMakeFiles/neural-network.dir/src/NeuralNetwork.cpp.o.d"
+  "CMakeFiles/neural-network.dir/src/main.cpp.o"
+  "CMakeFiles/neural-network.dir/src/main.cpp.o.d"
+  "neural-network"
+  "neural-network.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neural-network.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
